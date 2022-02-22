8fb1e8fd4d67d224845aa4491e2dabcb05db63b4
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-02-22 22:56:31 +0100

    kome.html update, views.py update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'Kamila',
+        'title' : 'Blog post 1',
+        'content' : 'First post content',
+        'date_posted' : 'August 27, 2018'
+    },
+    {
+        'author' : 'Jesio',
+        'title' : 'Blog post 2',
+        'content' : 'second post content',
+        'date_posted' : 'August 28, 2018'
+    }
+]
+
 
 def home(request):
+    context = {
+        'posts':posts 
+    }
     return render(request, 'blog/home.html')
 
 
