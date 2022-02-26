c0ed76c60c06aa0786fcf9502c520c4b0e66c4de
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-02-26 11:26:35 +0100

    templates correction - title

--- blog/views.py
+++ blog/views.py
@@ -21,9 +21,9 @@ def home(request):
     context = {
         'posts':posts 
     }
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About Page'})
 # Create your views here.
