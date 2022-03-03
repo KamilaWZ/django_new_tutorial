3c10a018cf8b6a40fbee0cea215e18c39b46f327
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-03 22:21:01 +0100

    admin.py modify, templatespy and views

--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,20 @@ from .models import Post
 
 #from django.http import HttpResponse
 
-posts = [
-    {
-        'author' : 'Kamila',
-        'title' : 'Blog post 1',
-        'content' : 'First post content',
-        'date_posted' : 'August 27, 2018'
-    },
-    {
-        'author' : 'Jesio',
-        'title' : 'Blog post 2',
-        'content' : 'second post content',
-        'date_posted' : 'August 28, 2018'
-    }
-]
+#posts = [
+   # {
+   #     'author' : 'Kamila',
+    #    'title' : 'Blog post 1',
+     #   'content' : 'First post content',
+      #  'date_posted' : 'August 27, 2018'
+   # },
+   # {
+    #    'author' : 'Jesio',
+     #   'title' : 'Blog post 2',
+      #  'content' : 'second post content',
+       # 'date_posted' : 'August 28, 2018'
+  #  }
+#]
 
 
 def home(request):
