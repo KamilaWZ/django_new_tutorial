66f333f55cb3f44c54fb3ae135c2a51c56731a2c
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-17 21:31:59 +0100

    modify settings and views.py, logout.html create

--- django_new_tutorial/settings.py
+++ django_new_tutorial/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL ='blog-home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
