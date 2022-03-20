14101a1a18a3a4d7e66cba9601f020fcf1d7fb7f
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-20 19:41:01 +0100

    delete

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,8 @@ from django.views.generic import (
     ListView, 
     DetailView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import Post
 
@@ -70,6 +71,15 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
     
 
 
