fa5626413dc807621811559076d7c74164de50d8
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-20 18:50:58 +0100

    models.py, views.py modify

--- blog/views.py
+++ blog/views.py
@@ -45,6 +45,10 @@ class PostDetailView(DetailView):
 class PostCreateView(CreateView):
     model = Post
     fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
     
     
 
