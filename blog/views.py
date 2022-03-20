04ff300d872156dc95d4ebaee3e52f25cf4e182e
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-20 17:29:17 +0100

    :blog/urls.py i views.py modify

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 #from django.http import HttpResponse
@@ -24,6 +25,12 @@ def home(request):
         'posts': Post.objects.all() 
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' 
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
 def about(request):
